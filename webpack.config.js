4367c7aa9a92ef68dfe01cf4636a12e680df3caf
Jurand76 <jurand@interia.pl>
2023-01-27 23:09:09 +0100

    feat: improve code

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,18 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8000, // Convert images < 8kb to base64 strings
+                            name: 'images/[hash]-[name].[ext]',
+                        },
+                    },
+                ],
+            },
         ],
     },
     resolve: {
