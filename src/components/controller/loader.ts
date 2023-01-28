af2d551008f6853095ffd02076dded84d68db5b5
Jurand76 <jurand@interia.pl>
2023-01-28 20:44:27 +0100

    feat: improve typescript code

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -16,7 +16,7 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res: any) {
+    errorHandler(res: Response) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -37,7 +37,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method: any, endpoint: string, callback: any, options = {}) {
+    load(method: any, endpoint: string, callback: Function, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
